baa84158397b36c1ad966caa7cb54a958eb4ba38
Nolan Hughes <nhughes987@gmail.com>
2017-12-07 15:17:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,6 +17,10 @@ class Student
 
   def self.create_from_collection(students_array)
     binding.pry
+    students_array.each do |hash| 
+      @name = hash[:name]
+      @locaiton = hash[:location]
+    end
   end
 
   def add_student_attributes(attributes_hash)
